054b08283d354bae6e2ac84e2098aeecd6ce3702
Edward Moreira <edward.moreira@yahoo.com>
2020-10-14 15:46:06 -0300

    :construction_worker: Creating and Formating the :round_pushpin:location page

---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,33 @@
+// Localization orphanage
+const orphanageSbo = [-22.743384, -47.4111377];
+
+// Create Map - setView([Latitude, longitide], zoom);
+const map = L.map("mapid").setView(orphanageSbo, 15);
+
+// Create amd Add TileLayer
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+// Create icon
+const icon = L.icon({
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
+
+// Create popup Overlay
+const popup = L.popup({
+  closeButton: false,
+  className: "map-popup",
+  minWidth: 240,
+  minHeight: 240,
+}).setContent(
+  'Casa da Criança <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg" ></a>'
+);
+
+// Create and add Marker
+L.marker(orphanageSbo, {
+  icon,
+}) /* Quando o valor é igual a propriedade não há necessidade de duplicarmos */
+  .addTo(map)
+  .bindPopup(popup);
